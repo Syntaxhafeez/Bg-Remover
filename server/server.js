cce0abf749711aeb71fc636b9f7eb225e31a60aa
Syntaxhafeez <syntaxhafeez@gmail.com>
2025-01-11 18:19:51 +0530

    `feat: Added Clerk Webhook Secret and User Controller for handling Clerk Webhooks`

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/MongoDb.js'
+import userRouter from './routes/userRoutes.js'
 
 
 // App Config
@@ -16,5 +17,7 @@ app.use(cors())
 
 // API Routes
 app.get('/', (req, res) => res.send("API Working"))
+app.use('/api/user', userRouter)
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
