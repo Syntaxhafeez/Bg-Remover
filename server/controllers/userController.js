245b04566f0b81d2c70404e214e3e20764c23be5
Syntaxhafeez <syntaxhafeez@gmail.com>
2025-01-12 23:58:51 +0530

    `Update plans and payment amounts in assets and userController`

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -112,19 +112,19 @@ const paymentRazorpay = async (req, res) => {
         switch (planId) {
             case 'Basic':
                 plan = 'Basic'
-                credits = 100
+                credits = 5
                 amount = 10
                 break;
 
             case 'Advanced':
                 plan = 'Advanced'
-                credits = 500
+                credits = 10
                 amount = 50
                 break;
 
             case 'Business':
                 plan = 'Business'
-                credits = 5000
+                credits = 50
                 amount = 250
                 break;
 
